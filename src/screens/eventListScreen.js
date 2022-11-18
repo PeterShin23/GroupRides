8336e309ebef2abaf62774250d8ff619c7f022be
Peter Shin <pss9fp@virginia.edu>
2022-11-18 02:34:39 -0500

    input for new event and new org screens; why is setting up modal so difficult

--- src/screens/eventListScreen.js
+++ src/screens/eventListScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 // import { darkTheme } from '../../utils/colors';
 
